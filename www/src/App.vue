860464aed547e65e03bc0f86a730956bc4619eb2
Ahmet Orhan <ahmetariforhan@gmail.com>
2018-03-07 12:16:48 +0300

    app.vue

--- www/src/App.vue
+++ www/src/App.vue
@@ -7,14 +7,6 @@
 
 <script>
 
-<<<<<<< HEAD
-console.log("nc");
-alert("asd")
-=======
-console.log("noteoo");
->>>>>>> cc2aaca93570a3e2bbfab66338b2c6a527f170c1
-
-console.log("1")
 
 export default {
   name: 'App'
