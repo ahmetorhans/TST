a3ce8cf3bb449a5a2f75075814ec51e686e95b35
Ahmet Orhan <ahmetariforhan@gmail.com>
2018-03-29 17:33:12 +0300

    2903

--- api/app/Http/Controllers/Upload.php
+++ api/app/Http/Controllers/Upload.php
@@ -20,11 +20,19 @@ class Upload extends Controller
      */
     public function uploadFile(Request $request){
         
-        $path = Storage::putFile('', $request->file('file')); 
+        $image = $request->file('file');
+        $filename  = time() . '.' . $image->getClientOriginalExtension();
+        $path = public_path('/app/files/'. $filename);
+        $thumb = public_path('app/files/thumb/'. $filename);
         
-        $image = Image::make('files/'.$path)->resize(300, 200)->save('files/thumb/'.$path);
-
-
-        return response()->json(array('status'=>true,'file'=>$path));
+        Image::make($image->getRealPath())->resize(1000, null, function ($constraint) {
+            $constraint->aspectRatio();
+        })->save($path,75);
+        Image::make($image->getRealPath())->resize(300, null, function ($constraint) {
+            $constraint->aspectRatio();
+        })->save($thumb,75);
+      //  $path = Storage::putFile('', $request->file('file')); 
+        //$image = Image::make('files/'.$path)->resize(300, 200)->save('files/thumb/'.$path);
+        return response()->json(array('status'=>true,'file'=>$filename));
     }
 }
